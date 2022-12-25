76ab5f9e4a68a8db5d1aa419a2f678cf53ccef17
adilrv <115145325+adilrv@users.noreply.github.com>
2022-12-26 00:32:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let searchBtn=document.querySelector('#search-btn')
+let searchBar=document.querySelector('#search-bar-container')
+searchBtn.addEventListener('click',()=>{
+searchBtn.classList.toggle('fa-times');
+});
